5a026c669f163af6f55715bb6cee311306399403
Javel Queiroz <javelqueiroz@outlook.com.br>
2022-10-30 15:38:06 -0300

    feat: add navsuperior structure

---
+++ src/components/commom/NavSuperior/index.jsx
@@ -0,0 +1,31 @@
+import React, { useCallback, useState } from 'react'
+import './style.css';
+
+export default function NavSuperior() {
+  let [buttonPressed, setButtonPressed] = useState(1);
+
+  let buttonClass = useCallback(
+    (button) => {
+      console.log(button, buttonPressed);
+      if(button !== buttonPressed) return '';
+      return '--pressed';
+    },
+    [buttonPressed],
+  )
+  
+  return (
+    <div className={`nav-superior__div`}>
+        <div>PIW Project</div>
+
+        <div className=''>
+          <button value={true} className={`nav-superior__button${buttonClass(1)}`}>
+              Linha do Tempo
+          </button>
+          <button className={`nav-superior__button${buttonClass(2)}`}>
+              Postar
+          </button>
+        </div>
+
+    </div>
+  )
+}
---
+++ src/components/pages/PaginaFeed/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavSuperior from '../../commom/NavSuperior'
+
+export default function PaginaFeed() {
+  return (
+    <div className='pagina-feed__div'>
+        <NavSuperior/>
+    </div>
+  )
+}
